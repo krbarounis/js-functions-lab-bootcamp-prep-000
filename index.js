571479bc6f978088913dc4ba3b36fef791d87591
Kristina Barounis <krbarounis@gmail.com>
2019-05-05 15:04:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function happyHolidayTo(holiday, name)
 {
   return `Happy ${holiday}, ${name}!`
 }
+//
+function holidayCountdown (holiday, days)
+{
+  return `It's ${days} days until ${holiday}!`
+}
